4a74774abe6cf66bb6c0f961ded3758b3dba237e
Amir Alibasic <alib.amir123@gmail.com>
2022-04-25 21:37:28 +0200

    Rick and Morty Wiki

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-center'>Rick and morty wiki</h1>
+    </div>
+  );
+}
+
+export default App;
